9500af4faf5a6b585f552d0d9b481f1ce060cdce
wind-of <adequation@mail.ru>
2023-10-10 06:52:31 +0300

    feat: simple task interface

--- src/components/canban/CanbanTableColumnList.vue
+++ src/components/canban/CanbanTableColumnList.vue
@@ -1,9 +1,17 @@
 <script setup>
-
+import CanbanTableColumnListItem from './CanbanTableColumnListItem.vue';
 </script>
 
 <template>
-	<section>
-		ColumnList
+	<section class="list">
+		<CanbanTableColumnListItem v-for="i in 3" :key="i"/>
 	</section>
 </template>
+
+<style scoped>
+.list {
+	display: flex;
+	flex-direction: column;
+	gap: 14px;
+}
+</style>
---
+++ src/components/canban/CanbanTableColumnListItem.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+</script>
+
+<template>
+	<section class="item">
+		<header class="header">
+			<div class="tag">Design</div>
+		</header>
+		<section class="text">
+			<h3 class="title">Task title</h3>
+			<p class="description">Description</p>
+		</section>
+	</section>
+</template>
+
+<style lang="scss" scoped>
+.item {
+	@include flex-column;
+	gap: 15px;
+	padding: 16px;
+	border-radius: 12px; 
+	background-color: var(--white);
+}
+.tag {
+	display: inline-block;
+	padding: 3px 9px;
+	border-radius: 4px;
+	background-color: var(--blue);
+	font-size: 12px;
+	font-weight: 500;
+	color: var(--white);
+}
+.text {
+	@include flex-column;
+	font-weight: 500;
+	gap: 7px;
+}
+.title {
+	font-size: 13px;
+}
+.description {
+	font-size: 12px;
+	color: var(--subtitle);
+}
+</style>
