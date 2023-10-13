3931238f2419d2fd6d437c1ab424642691f69d33
wind-of <adequation@mail.ru>
2023-10-13 13:19:48 +0300

    feat: улучшенные mock-данные

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,29 +1,16 @@
 const rn = () => ((Math.random() * 1000) | 0).toString().padStart(4, (Math.random() * 10) | 0)
 const uid = (prefix) => `${prefix}-${rn()}-${rn()}-${rn()}-${rn()}`
+
+const tag = (title = "Design") => ({ id: uid("tag"), title })
+const task = (title = "Task", description = "Description", tags = generateTags(1)) => ({ id: uid("tag"), title, description, tags })
+const column = (title = "Column", tasks = generateTasks(3)) => ({ id: uid("tag"), title, tasks })
+
+const generateTags = (length) => Array.from({ length }, () => tag()) 
+const generateTasks = (length) => Array.from({ length }, () => task()) 
+const generateColumns = (length) => Array.from({ length }, () => column()) 
+
 export const project = {
   id: uid("project"),
   title: "Project Title",
-  columns: [
-    {
-      id: uid("column"),
-      title: "Column Title",
-      tasks: [
-        {
-          id: uid("task"),
-          title: "Task title",
-          description: "Tasks description",
-          tags: [
-            {
-              id: uid("tag"),
-              title: "Design"
-            },
-            {
-              id: uid("tag"),
-              title: "Frontend"
-            }
-          ]
-        }
-      ]
-    }
-  ]
+  columns: generateColumns(3)
 }
