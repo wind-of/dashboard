a2da941395513cf23e93c795949c942b25309171
wind-of <adequation@mail.ru>
2023-10-15 10:03:30 +0300

    mock: небольшие правки в идентификаторы мок-данных

--- src/mock/index.js
+++ src/mock/index.js
@@ -3,8 +3,8 @@ const rs = () => rn().toString().padStart(4, (Math.random() * 10) | 0)
 const uid = (prefix) => `${prefix}-${rs()}-${rs()}-${rs()}-${rs()}`
 
 const tag = (title = "Design #" + rn()) => ({ id: uid("tag"), title })
-const task = (title = "Task #" + rn(), description = "Description", tags = generateTags(1)) => ({ id: uid("tag"), title, description, tags })
-const column = (title = "Column #" + rn(), tasks = generateTasks(3)) => ({ id: uid("tag"), title, tasks })
+const task = (title = "Task #" + rn(), description = "Description", tags = generateTags(1)) => ({ id: uid("task"), title, description, tags })
+const column = (title = "Column #" + rn(), tasks = generateTasks(3)) => ({ id: uid("table"), title, tasks })
 
 const generateTags = (length) => Array.from({ length }, () => tag()) 
 const generateTasks = (length) => Array.from({ length }, () => task()) 
