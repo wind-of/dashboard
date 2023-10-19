218e16873104ba1b272484fa94fdeca3eeea497d
wind-of <adequation@mail.ru>
2023-10-19 07:38:00 +0300

    feat: базовые компоненты VInput и VTextarea

---
+++ src/components/form/VInput.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { defineModel } from "vue"
+const modelValue = defineModel()
+</script>
+
+<template>
+  <input class="input" type="text" v-model="modelValue" />
+</template>
+
+<style lang="scss" scoped>
+.input {
+  @include input;
+}
+</style>
---
+++ src/components/form/VTextarea.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { defineModel } from "vue"
+const modelValue = defineModel()
+</script>
+
+<template>
+  <textarea class="textarea" v-model="modelValue" />
+</template>
+
+<style lang="scss" scoped>
+.textarea {
+  @include textarea;
+}
+</style>
