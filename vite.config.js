531e3f51dab58f1f82fa8470ea804a4fc645755c
wind-of <adequation@mail.ru>
2023-10-19 07:37:24 +0300

    build: включил defineModel

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url"
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  vite: {
+    vue: {
+      script: {
+        defineModel: true
+      }
+    }
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url))
     }
   },
   css: {
@@ -19,5 +24,5 @@ export default defineConfig({
         additionalData: `@import "@/assets/global.scss";`
       }
     }
-  },
+  }
 })
