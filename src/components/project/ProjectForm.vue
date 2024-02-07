06128bb94e7a9e44f854f8773ee1ed0398a47e88
wind-of <adequation@mail.ru>
2024-02-07 11:39:03 +0300

    feat: возможность менять название проекта на странице проекта

--- src/components/project/ProjectForm.vue
+++ src/components/project/ProjectForm.vue
@@ -1,207 +1,53 @@
 <script setup lang="ts">
-import { computed, reactive, watchEffect } from "vue"
+import { computed, ref } from "vue"
 import { storeToRefs } from "pinia"
-import type { UserWithRole } from "@/types"
-import { ParticipantRolesEnum } from "@/types"
 import { useProjectStore } from "@/stores/project"
-import { useCopyReactive } from "@/composables/copy.reactive"
-import { getUsersByIds } from "@/api/user.requests"
 import InputBlock from "@/components/form/InputBlock.vue"
-import VAvatar from "@/components/common/VAvatar.vue"
-import VSelect from "@/components/form/VSelect.vue"
-import { ParticipantRoles } from "@/constants"
-import { keyMapper } from "@/utils"
-import { deleteParticipantRequest, updateParticipantsRole } from "@/api/participants.requests"
-import { useUserStore } from "@/stores/user"
+import ProjectFormParticipants from "@/components/project/ProjectFormParticipants.vue"
+import { updateProjectTitle } from "@/api"
 
-const userStore = useUserStore()
 const projectStore = useProjectStore()
 const { project } = storeToRefs(projectStore)
 
-const plainParticipants = computed(() => project.value?.participants)
-
-const roles = computed(
-  () => plainParticipants.value && keyMapper(plainParticipants.value, "userId", "role")
-)
-const form = computed(() => ({
-  state: reactive({
-    ...useCopyReactive(project.value),
-    participants: [],
-    participantRoles: {}
-  })
-}))
-const state = computed(() => form.value.state)
-
-watchEffect(async () => {
-  if (!plainParticipants.value) {
-    return
-  }
-  const { data: users } = await getUsersByIds(plainParticipants.value.map(({ userId }) => userId))
-  state.value.participants = users.map((user) => ({ ...user, role: roles.value![user.id] }))
-  state.value.participantRoles = keyMapper(plainParticipants.value, "userId", "role")
-})
-
-function shouldDisableRolesSelect(role: ParticipantRolesEnum) {
-  const currentUserRole = roles.value?.[userStore.user?.id as number] as ParticipantRolesEnum
-  return (
-    currentUserRole === ParticipantRolesEnum.Member ||
-    role === ParticipantRolesEnum.Owner ||
-    (role === ParticipantRolesEnum.Admin && currentUserRole !== ParticipantRolesEnum.Owner)
-  )
-}
-function isRolesOperationPermissible(role: ParticipantRolesEnum) {
-  const currentUserRole = roles.value?.[userStore.user?.id as number] as ParticipantRolesEnum
-  return (
-    currentUserRole === ParticipantRolesEnum.Owner ||
-    (currentUserRole === ParticipantRolesEnum.Admin && role === ParticipantRolesEnum.Admin)
-  )
-}
-function isUserDeletionPermissible(role: ParticipantRolesEnum) {
-  const currentUserRole = roles.value?.[userStore.user?.id as number] as ParticipantRolesEnum
-  return (
-    currentUserRole === ParticipantRolesEnum.Owner ||
-    (currentUserRole === ParticipantRolesEnum.Admin && role === ParticipantRolesEnum.Member)
-  )
-}
-
-async function handleParticipantsRoleUpdate(participantId: number) {
-  await updateParticipantsRole(
-    project.value?.id as number,
-    participantId,
-    state.value.participantRoles[participantId]
-  )
-  await projectStore.updateProjectInStore(project.value?.id as number)
-}
-async function handleParticipantsDeletion(participantId: number) {
-  await deleteParticipantRequest(project.value?.id as number, participantId)
+const title = ref(project.value?.title || "")
+const doesTitleChanged = computed(() => title.value !== project.value?.title)
+async function handleFormSubmit() {
+  await updateProjectTitle(project.value?.id as number, title.value)
   await projectStore.updateProjectInStore(project.value?.id as number)
 }
 </script>
 
 <template>
-  <form class="form" @submit.prevent>
-    <InputBlock class="input" v-model="state.title" label="Title" />
-    <section class="users-wrapper">
-      <h2>Users</h2>
-      <ul class="user-list">
-        <li
-          v-for="participant in state.participants as UserWithRole[]"
-          :key="participant.id"
-          class="user-list-item"
-        >
-          <VAvatar class="user-data" :image="participant.avatar" />
-          <section class="user-data user-name">
-            {{ participant.firstname }} {{ participant.lastname }}
-          </section>
-          <section class="user-data user-email">{{ participant.email }}</section>
-          <VSelect
-            v-model="state.participantRoles[participant.id]"
-            :defaultTitle="participant.role"
-            :disabled="shouldDisableRolesSelect(participant.role)"
-          >
-            <option v-for="role in ParticipantRoles" :key="role" :value="role">
-              {{ role }}
-            </option>
-          </VSelect>
-          <section class="user-data button-wrapper">
-            <button
-              v-if="state.participantRoles[participant.id] !== participant.role"
-              class="button"
-              :class="{
-                'button-disabled': !isRolesOperationPermissible(
-                  state.participantRoles[participant.id]
-                )
-              }"
-              @click="handleParticipantsRoleUpdate(participant.id)"
-              :disabled="!isRolesOperationPermissible(state.participantRoles[participant.id])"
-            >
-              Save
-            </button>
-          </section>
-          <section class="user-data button-wrapper">
-            <button
-              class="button button-delete"
-              :class="{
-                'button-disabled': !isUserDeletionPermissible(participant.role)
-              }"
-              :disabled="!isUserDeletionPermissible(participant.role)"
-              @click="handleParticipantsDeletion(participant.id)"
-            >
-              Delete
-            </button>
-          </section>
-        </li>
-      </ul>
+  <form class="form" @submit.prevent="handleFormSubmit">
+    <section class="title-section">
+      <InputBlock class="input" v-model="title" label="Title" />
+      <button :class="{ disabled: !doesTitleChanged }" :disabled="!doesTitleChanged">Save</button>
     </section>
+    <ProjectFormParticipants />
   </form>
 </template>
 
 <style scoped lang="scss">
 .form {
   width: 80vw;
-  max-width: 700px;
+  max-width: 800px;
   display: flex;
   flex-direction: column;
 }
-.input {
+.title-section {
+  @include flex-row;
+  gap: 30px;
+  align-items: flex-end;
   margin-bottom: 40px;
-}
-
-.users-wrapper {
-  display: flex;
-  flex-direction: column;
-  gap: 40px;
-  margin-bottom: 30px;
-}
-
-.user-list {
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-}
-.user-list-item {
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  gap: 15px;
-}
-.user-data {
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  font-size: 18px;
-  &.user-role {
-    width: 10%;
-  }
-  &.user-name {
-    width: 25%;
-  }
-  &.user-email {
-    width: 50%;
-  }
-  &.button-wrapper {
-    width: 15%;
-  }
-}
-
-.button {
-  width: 100%;
-  font-size: 14px;
-  border-radius: 10px;
-  background: var(--blue);
-  color: white;
-
-  &.button-disabled {
-    opacity: 0.5;
-    cursor: not-allowed;
-  }
-
-  &.button-delete {
-    border: 1px solid var(--red);
-    border-radius: 5px;
-    background: transparent;
-    color: var(--red);
+  button {
+    font-size: 14px;
+    font-weight: 500;
+    background-color: var(--blue);
+    color: white;
+    &.disabled {
+      opacity: 0.5;
+      cursor: not-allowed;
+    }
   }
 }
 </style>
---
+++ src/components/project/ProjectFormParticipants.vue
@@ -0,0 +1,188 @@
+<script setup lang="ts">
+import { computed, reactive, watchEffect } from "vue"
+import { storeToRefs } from "pinia"
+import type { UserWithRole } from "@/types"
+import { ParticipantRolesEnum } from "@/types"
+import { useProjectStore } from "@/stores/project"
+import { getUsersByIds } from "@/api/user.requests"
+import VAvatar from "@/components/common/VAvatar.vue"
+import VSelect from "@/components/form/VSelect.vue"
+import { ParticipantRoles } from "@/constants"
+import { keyMapper } from "@/utils"
+import { deleteParticipantRequest, updateParticipantsRole } from "@/api/participants.requests"
+import { useUserStore } from "@/stores/user"
+
+const userStore = useUserStore()
+const projectStore = useProjectStore()
+const { project } = storeToRefs(projectStore)
+
+const plainParticipants = computed(() => project.value?.participants)
+
+const roles = computed(
+  () => plainParticipants.value && keyMapper(plainParticipants.value, "userId", "role")
+)
+const form = computed(() => ({
+  state: reactive({
+    participants: [],
+    participantRoles: {}
+  })
+}))
+const state = computed(() => form.value.state)
+
+watchEffect(async () => {
+  if (!plainParticipants.value) {
+    return
+  }
+  const { data: users } = await getUsersByIds(plainParticipants.value.map(({ userId }) => userId))
+  state.value.participants = users.map((user) => ({ ...user, role: roles.value![user.id] }))
+  state.value.participantRoles = keyMapper(plainParticipants.value, "userId", "role")
+})
+
+function shouldDisableRolesSelect(role: ParticipantRolesEnum) {
+  const currentUserRole = roles.value?.[userStore.user?.id as number] as ParticipantRolesEnum
+  return (
+    currentUserRole === ParticipantRolesEnum.Member ||
+    role === ParticipantRolesEnum.Owner ||
+    (role === ParticipantRolesEnum.Admin && currentUserRole !== ParticipantRolesEnum.Owner)
+  )
+}
+function isRolesOperationPermissible(role: ParticipantRolesEnum) {
+  const currentUserRole = roles.value?.[userStore.user?.id as number] as ParticipantRolesEnum
+  return (
+    currentUserRole === ParticipantRolesEnum.Owner ||
+    (currentUserRole === ParticipantRolesEnum.Admin && role === ParticipantRolesEnum.Admin)
+  )
+}
+function isUserDeletionPermissible(role: ParticipantRolesEnum) {
+  const currentUserRole = roles.value?.[userStore.user?.id as number] as ParticipantRolesEnum
+  return (
+    currentUserRole === ParticipantRolesEnum.Owner ||
+    (currentUserRole === ParticipantRolesEnum.Admin && role === ParticipantRolesEnum.Member)
+  )
+}
+
+async function handleParticipantsRoleUpdate(participantId: number) {
+  await updateParticipantsRole(
+    project.value?.id as number,
+    participantId,
+    state.value.participantRoles[participantId]
+  )
+  await projectStore.updateProjectInStore(project.value?.id as number)
+}
+async function handleParticipantsDeletion(participantId: number) {
+  await deleteParticipantRequest(project.value?.id as number, participantId)
+  await projectStore.updateProjectInStore(project.value?.id as number)
+}
+</script>
+
+<template>
+  <section class="participants-wrapper">
+    <h2>Participants</h2>
+    <ul class="participants-list">
+      <li
+        v-for="participant in state.participants as UserWithRole[]"
+        :key="participant.id"
+        class="participants-list-item"
+      >
+        <VAvatar class="participants-data" :image="participant.avatar" />
+        <section class="participants-data participants-name">
+          {{ participant.firstname }} {{ participant.lastname }}
+        </section>
+        <section class="participants-data participants-email">{{ participant.email }}</section>
+        <VSelect
+          v-model="state.participantRoles[participant.id]"
+          :defaultTitle="participant.role"
+          :disabled="shouldDisableRolesSelect(participant.role)"
+        >
+          <option v-for="role in ParticipantRoles" :key="role" :value="role">
+            {{ role }}
+          </option>
+        </VSelect>
+        <section class="participants-data button-wrapper">
+          <button
+            v-if="state.participantRoles[participant.id] !== participant.role"
+            class="button"
+            :class="{
+              'button-disabled': !isRolesOperationPermissible(
+                state.participantRoles[participant.id]
+              )
+            }"
+            @click="handleParticipantsRoleUpdate(participant.id)"
+            :disabled="!isRolesOperationPermissible(state.participantRoles[participant.id])"
+          >
+            Save
+          </button>
+        </section>
+        <section class="participants-data button-wrapper">
+          <button
+            class="button button-delete"
+            :class="{
+              'button-disabled': !isUserDeletionPermissible(participant.role)
+            }"
+            :disabled="!isUserDeletionPermissible(participant.role)"
+            @click="handleParticipantsDeletion(participant.id)"
+          >
+            Delete
+          </button>
+        </section>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.participants-wrapper {
+  @include flex-column;
+  gap: 40px;
+  margin-bottom: 30px;
+}
+.participants-list {
+  @include flex-column;
+  gap: 20px;
+}
+.participants-list-item {
+  width: 100%;
+  @include flex-row;
+  align-items: center;
+  justify-content: space-between;
+  gap: 15px;
+}
+.participants-data {
+  @include flex-row;
+  align-items: center;
+  justify-content: space-around;
+  font-size: 18px;
+  &.participants-role {
+    width: 10%;
+  }
+  &.participants-name {
+    width: 25%;
+  }
+  &.participants-email {
+    width: 50%;
+  }
+  &.button-wrapper {
+    width: 15%;
+  }
+}
+
+.button {
+  width: 100%;
+  font-size: 14px;
+  border-radius: 10px;
+  background: var(--blue);
+  color: white;
+
+  &.button-disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+
+  &.button-delete {
+    border: 1px solid var(--red);
+    border-radius: 5px;
+    background: transparent;
+    color: var(--red);
+  }
+}
+</style>
